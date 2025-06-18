3783941766001c26f779a46fca60fbb808292905
Santyrpdv <santyrpdv@gmail.com>
2025-06-18 01:23:18 -0300

    Reorganizacion y agregacion de infra

---
+++ .infra/terraform/terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"   
+  
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks_auth.token
+  }
+}
