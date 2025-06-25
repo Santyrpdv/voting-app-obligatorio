33ac45d8dfc28e0920ebbcdf1db10d39a1094d24
Santyrpdv <santyrpdv@gmail.com>
2025-06-25 14:47:06 -0300

    Se agrega bucket S3 para gardar estado de terraform

--- .infra/terraform/backend.tf
+++ .infra/terraform/backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket         = "obligatorio-sparis-301463-terraform-remote-statef"
     key            = "voteApp-eks/terraform.tfstate"
     region         = "us-east-1"
-    profile        = "default"
   }
 }
