57bb9d08fa9183402bec11b2d14a950ddef83115
Santyrpdv <santyrpdv@gmail.com>
2025-06-28 23:55:43 -0300

    Se ignora lambda para resovler errores

--- .infra/terraform/lambda.tf
+++ .infra/terraform/lambda.tf
@@ -1,28 +1,28 @@
-data "kubernetes_service" "ingress_nginx" {
-  metadata {
-    name      = "ingress-nginx-controller"
-    namespace = "ingress-nginx"
-  }
-}
+# data "kubernetes_service" "ingress_nginx" {
+#   metadata {
+#     name      = "ingress-nginx-controller"
+#     namespace = "ingress-nginx"
+#   }
+# }
 
-locals {
-  alb_hostname = data.kubernetes_service.ingress_nginx.status[0].load_balancer[0].ingress[0].hostname
-}
+# locals {
+#   alb_hostname = data.kubernetes_service.ingress_nginx.status[0].load_balancer[0].ingress[0].hostname
+# }
 
-resource "aws_lambda_function" "healthcheck" {
-  function_name = "healthcheck"
-  role          = data.aws_iam_role.lab_role.arn
-  handler       = "main.handler"
-  runtime       = "python3.11"
-  filename      = "healthcheck-lambda.zip"
-  timeout       = 30
-  source_code_hash = filebase64sha256("healthcheck-lambda.zip")
+# resource "aws_lambda_function" "healthcheck" {
+#   function_name = "healthcheck"
+#   role          = data.aws_iam_role.lab_role.arn
+#   handler       = "main.handler"
+#   runtime       = "python3.11"
+#   filename      = "healthcheck-lambda.zip"
+#   timeout       = 30
+#   source_code_hash = filebase64sha256("healthcheck-lambda.zip")
 
-  environment {
-    variables = {
-      ALB_HOSTNAME = local.alb_hostname
-    }
-  }
+#   environment {
+#     variables = {
+#       ALB_HOSTNAME = local.alb_hostname
+#     }
+#   }
 
-  depends_on = [helm_release.nginx_ingress]
-}
+#   depends_on = [helm_release.nginx_ingress]
+# }
