f61238ede9decfc0f52295146c8248f476acdb41
Santyrpdv <santyrpdv@gmail.com>
2025-06-30 19:10:30 -0300

    Se agrega readme al proyecto

--- .infra/terraform/backend.tf
+++ .infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "obligatorio-sparis-301463-terraform-remote-statef"
+    bucket         = "obligatorio-sparis-301463-terraform-remote-statef${var.environment}"
     key            = "voteApp-eks/terraform.tfstate"
     region         = "us-east-1"
   }
